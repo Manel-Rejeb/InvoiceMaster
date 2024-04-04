cd6ea8d476e576cb098ae868c793542cc54283ca
Manel Rejeb <91155171+Manel-Rejeb@users.noreply.github.com>
2024-04-04 03:22:03 +0100

    creating the article CRUD

---
+++ src/articles/articles.service.ts
@@ -0,0 +1,50 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { Article } from './entities/article.entity';
+
+@Injectable()
+export class ArticlesService {
+  constructor(
+    @InjectRepository(Article)
+    private readonly articleRepository: Repository<Article>,
+  ) {}
+
+  async create(createArticle: Article): Promise<Article> {
+    const newArticle = await this.articleRepository.create(createArticle);
+    return this.articleRepository.save(newArticle);
+  }
+
+  async findAll(): Promise<Article[]> {
+    return this.articleRepository.find();
+  }
+
+  async findOne(id: number): Promise<Article> {
+    const articleData = await this.articleRepository.findOneBy({ id });
+    if (!articleData) {
+      throw new NotFoundException('Article Not Found');
+    }
+    return articleData;
+  }
+
+  async update(id: number, updateArticle: Article): Promise<Article> {
+    const articleData = await this.articleRepository.findOneBy({ id });
+    if (!articleData) {
+      throw new NotFoundException('Article Not Found');
+    }
+    const updatedArticle = this.articleRepository.merge(
+      articleData,
+      updateArticle,
+    );
+    return await this.articleRepository.save(articleData);
+  }
+
+  async remove(id: number): Promise<Article> {
+    const articleData = await this.articleRepository.findOneBy({ id });
+    if (!articleData) {
+      throw new NotFoundException('User not found');
+    }
+    return await this.articleRepository.remove(articleData);
+  }
+}
---
+++ src/articles/entities/enum/ARTICLE_UNIT.ts
@@ -0,0 +1,6 @@
+export enum ARTICLE_UNIT {
+  HOURLY = 'HOURLY',
+  DAILY = 'DAILY',
+  PROJECT = 'PROJECT',
+  QUANTITY = 'QUANTITY',
+}
