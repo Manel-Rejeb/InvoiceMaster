ce3a24b11ff0f1ec7e3794f76edda738745bb5fe
Manel Rejeb <91155171+Manel-Rejeb@users.noreply.github.com>
2024-05-19 18:02:38 +0100

    dashboard layout

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,6 +1,23 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import '@/styles/globals.css'
+import type { AppProps } from 'next/app'
+
+import { useRouter } from 'next/router'
+
+import { ConfigProvider } from 'antd/lib'
+import { DashboardLayout } from '@/layout/dashboard.layout'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const { pathname } = useRouter()
+
+  if (pathname.includes('/dashboard')) {
+    return (
+      <ConfigProvider theme={{}}>
+        <DashboardLayout>
+          <Component {...pageProps} />
+        </DashboardLayout>
+      </ConfigProvider>
+    )
+  }
+
+  return <ConfigProvider theme={{}} ><Component {...pageProps} /></ConfigProvider>
 }
