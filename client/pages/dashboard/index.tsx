f77d96776ef921ea498bd5ffdeef9c3dd78dff7c
Manel Rejeb <91155171+Manel-Rejeb@users.noreply.github.com>
2024-05-19 17:58:15 +0100

    dashboard configuration

--- client/pages/dashboard/index.tsx
+++ client/pages/dashboard/index.tsx
@@ -1,9 +1,8 @@
 import {type JSX} from 'react'
 
 export default function Dashboard() : JSX.Element {
-    return (
-        <div className="flex items-center justify-center h-screen">
-            <h1>dashboard page</h1>
-        </div>
-        )
+	return (
+    <div className="flex items-center justify-center h-screen">
+      <h1>dashboard page</h1>
+			</div>)
 }
--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -1,9 +1,13 @@
 import type { Config } from 'tailwindcss'
+import { COLORS } from './styles/theme/tailwind-antd-colors'
 
 const config: Config = {
   darkMode: ['class'],
-  content: ['./pages/**/*.{js,ts,jsx,tsx,mdx}', './components/**/*.{js,ts,jsx,tsx,mdx}', './app/**/*.{js,ts,jsx,tsx,mdx}'],
-  theme: {},
-  plugins: [],
+  content: ['./pages/**/*.{js,ts,jsx,tsx,mdx}', './layout/**/*.{js,ts,jsx,tsx,mdx}', './components/**/*.{js,ts,jsx,tsx,mdx}'],
+  theme: {
+    extend: {
+      colors: { transparent: 'transparent', current: 'currentColor', ...COLORS },
+    },
+  },
 }
 export default config
