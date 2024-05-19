0ebe28f9db46b2e008dd098e7ca3166362e2e73a
Manel Rejeb <91155171+Manel-Rejeb@users.noreply.github.com>
2024-05-19 22:16:50 +0100

    updating dashboard layout

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -3,7 +3,7 @@ import { COLORS } from './styles/theme/tailwind-antd-colors'
 
 const config: Config = {
   darkMode: ['class'],
-  content: ['./pages/**/*.{js,ts,jsx,tsx,mdx}', './layout/**/*.{js,ts,jsx,tsx,mdx}', './components/**/*.{js,ts,jsx,tsx,mdx}'],
+  content: ['./pages/**/*.{js,ts,jsx,tsx,mdx}', './layouts/**/*.{js,ts,jsx,tsx,mdx}', './components/**/*.{js,ts,jsx,tsx,mdx}', './blueprints/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
       colors: { transparent: 'transparent', current: 'currentColor', ...COLORS },
