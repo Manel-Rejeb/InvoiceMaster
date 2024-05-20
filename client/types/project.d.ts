4f18ce0f9e5f97e1b80b918a575c6ea7a14bfbe0
Manel Rejeb <91155171+Manel-Rejeb@users.noreply.github.com>
2024-05-21 00:41:40 +0100

    customer data table

---
+++ client/types/project.d.ts
@@ -0,0 +1,11 @@
+type ProjectType = {
+  id: number
+  project_name: string
+  project_description: string
+  project_start_date: string
+  project_end_date: string
+  project_status: string
+  createdAt: string
+  updatedAt: string
+  isSoftDelete: boolean
+}
